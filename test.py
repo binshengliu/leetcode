b927253b4f75b41b5d78d9c3eac8c8d1659e3663
Liu Binsheng <liubinsheng@gmail.com>
2017-02-10 21:23:52 +0800

    add test.py as template

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+import code
+
+class TestSolution(unittest.TestCase):
+    def test_1(self):
+        s = code.Solution()
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
