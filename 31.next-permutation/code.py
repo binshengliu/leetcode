6fb4a0c18c283a6a8123e69b9554e8c1c2fa7737
Liu Binsheng <liubinsheng@gmail.com>
2017-03-07 23:12:40 +0800

    solve #31 in O(n)

--- 31.next-permutation/code.py
+++ 31.next-permutation/code.py
@@ -1,19 +1,33 @@
 # find from rear to head, the first element that is smaller than any one of the digit after it
 
 class Solution(object):
+    def reverse(self, nums, begin, end):
+        l = begin
+        r = end - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
     def nextPermutation(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        for (i, num) in reversed(list(enumerate(nums))):
+        swap = len(nums) - 2
+        while swap >= 0:
+            if nums[swap] < nums[swap + 1]:
+                break
+            swap -= 1
 
-            for begin in range(i + 1, len(nums)):
-                if num < nums[begin]:
-                    nums[i], nums[begin] = nums[begin], nums[i]
-                    return
+        if swap >= 0:
+            to_swap = swap
+            while to_swap <= len(nums) - 2:
+                if nums[to_swap + 1] <= nums[swap] < nums[to_swap]:
+                    break
+                to_swap += 1
 
-            for begin in range(i + 1, len(nums)):
-                nums[begin - 1] = nums[begin]
-
-            nums[len(nums) - 1] = num
+            nums[swap], nums[to_swap] = nums[to_swap], nums[swap]
+            self.reverse(nums, swap + 1, len(nums))
+        else:
+            self.reverse(nums, 0, len(nums))
