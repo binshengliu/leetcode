9f4df7506748e1c8d27e80e28aba5537cbc4273e
Liu Binsheng <liubinsheng@gmail.com>
2017-03-28 17:26:57 +0800

    conform naming conventions

--- 37.sudoku-solver/code.py
+++ 37.sudoku-solver/code.py
@@ -1,33 +1,34 @@
 # from test import print_board
 
+
 class Solution(object):
     def __init__(self):
         self._row_vals = [set() for _ in range(0, 9)]
         self._col_vals = [set() for _ in range(0, 9)]
         self._block_vals = [set() for _ in range(0, 9)]
 
-    def toBlockId(self, row, col):
+    def __toBlockId(self, row, col):
         block_id = int(row / 3) * 3 + int(col / 3)
         return block_id
 
-    def toRowCol(self, block_id):
+    def __toRowCol(self, block_id):
         row_begin = int(block_id / 3) * 3
         col_begin = int(block_id % 3) * 3
         return (row_begin, col_begin)
 
-    def addValue(self, row, col, val):
-        block_id = self.toBlockId(row, col)
+    def __addValue(self, row, col, val):
+        block_id = self.__toBlockId(row, col)
         self._row_vals[row].add(val)
         self._col_vals[col].add(val)
         self._block_vals[block_id].add(val)
 
-    def removeValue(self, row, col, val):
-        block_id = self.toBlockId(row, col)
+    def __removeValue(self, row, col, val):
+        block_id = self.__toBlockId(row, col)
         self._row_vals[row].remove(val)
         self._col_vals[col].remove(val)
         self._block_vals[block_id].remove(val)
 
-    def isValueValid(self, row, col, val):
+    def __isValueValid(self, row, col, val):
         '''Test if val is valid'''
         if val in self._row_vals[row]:
             return False
@@ -35,13 +36,13 @@ class Solution(object):
         if val in self._col_vals[col]:
             return False
 
-        block_id = self.toBlockId(row, col)
+        block_id = self.__toBlockId(row, col)
         if val in self._block_vals[block_id]:
             return False
 
         return True
 
-    def nextPosition(self, row, col):
+    def __nextPosition(self, row, col):
         '''Find next position'''
         next_row = row
         next_col = (col + 1) % 9
@@ -49,35 +50,35 @@ class Solution(object):
             next_row = row + 1
         return (next_row, next_col)
 
-    def solveSudokuRec(self, board, row, col):
+    def __solveSudokuRec(self, board, row, col):
         # goes beyond the last row
         if row == 9:
             # All done
             return True
 
-        next_row, next_col = self.nextPosition(row, col)
+        next_row, next_col = self.__nextPosition(row, col)
 
         if board[row][col] == '.':
             # print('\nrow: {}, col: {}'.format(row, col))
             # print_board(board)
             for val in '123456789':
-                is_valid = self.isValueValid(row, col, val)
+                is_valid = self.__isValueValid(row, col, val)
                 if is_valid:
                     # print('{} is valid at {}, {}'.format(val, row, col))
                     board[row][col] = val
-                    self.addValue(row, col, val)
+                    self.__addValue(row, col, val)
 
                     # Process next position
-                    ans = self.solveSudokuRec(board, next_row, next_col)
+                    ans = self.__solveSudokuRec(board, next_row, next_col)
                     if ans:
                         return True
                     else:
                         board[row][col] = '.'
-                        self.removeValue(row, col, val)
+                        self.__removeValue(row, col, val)
                 # else:
                 #     print('{} is invalid at {}, {}'.format(val, row, col))
         else:
-            ans = self.solveSudokuRec(board, next_row, next_col)
+            ans = self.__solveSudokuRec(board, next_row, next_col)
             return ans
 
         # print('unable to continue at row {} col {}'.format(row, col))
@@ -89,19 +90,24 @@ class Solution(object):
         :rtype: void Do not return anything, modify board in-place instead.
         """
         for row in range(0, 9):
-            self._row_vals[row] = set([val for val in board[row] if val != '.'])
+            self._row_vals[row] = set(
+                [val for val in board[row] if val != '.'])
 
         for col in range(0, 9):
-            self._col_vals[col] = set([board[row][col] for row in range(0, 9) if board[row][col] != '.'])
+            self._col_vals[col] = set([
+                board[row][col] for row in range(0, 9)
+                if board[row][col] != '.'
+            ])
 
         for block in range(0, 9):
-            (row_begin, col_begin) = self.toRowCol(block)
+            (row_begin, col_begin) = self.__toRowCol(block)
             row_end = row_begin + 3
             col_end = col_begin + 3
-            self._block_vals[block] = set([board[row][col] \
-                                           for row in range(row_begin, row_end) \
-                                           for col in range(col_begin, col_end) \
-                                           if board[row][col] != '.'])
+            self._block_vals[block] = set([
+                board[row][col]
+                for row in range(row_begin, row_end)
+                for col in range(col_begin, col_end) if board[row][col] != '.'
+            ])
 
-        self.solveSudokuRec(board, 0, 0)
+        self.__solveSudokuRec(board, 0, 0)
         # print_board(board)
