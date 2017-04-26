93c0b7ab20c416ef8af2660362565d78addf8349
Liu Binsheng <liubinsheng@gmail.com>
2017-04-26 20:51:04 +0800

    solve #60 not using brute force

--- 60.permutation-sequence/solution.py
+++ 60.permutation-sequence/solution.py
@@ -1,17 +1,33 @@
+import math
+
+
 class Solution(object):
-    def getPermutationRecursively(self, l, k):
-        if not l:
-            return [[]]
+    def genFactorial(self, n):
+        self._factorial = {}
+        val = 1
+        for i in range(1, n+1):
+            val *= i
+            self._factorial[i] = val
 
-        ans = []
-        for (i, num) in enumerate(l):
-            first = num
-            rest = l[:i] + l[i+1:]
-            sub_ans = self.getPermutationRecursively(rest, k)
-            for one_list in sub_ans:
-                one_list.insert(0, first)
-            ans.extend(sub_ans)
+    # digit_index starts from 1, starting from most significant digit
+    def getPermutationRecursively(self, n, l, k, digit_index):
+        if n == digit_index:
+            return str(l[0])
 
+        candidates_per_digit = self._factorial[n - digit_index]
+
+        # The float conversion is a workaround for leetcode
+        current_index = int(math.ceil(float(k) / candidates_per_digit)) - 1
+
+        # list indes starts from 0, so minus 1
+        current_value = l[current_index]
+
+        l.remove(current_value)
+        sub_ans = self.getPermutationRecursively(n, l,
+                                                 k % candidates_per_digit,
+                                                 digit_index+1)
+
+        ans = str(current_value) + sub_ans
         return ans
 
     def getPermutation(self, n, k):
@@ -20,8 +36,9 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
+        self.genFactorial(n)
         l = [i for i in range(1, n+1)]
 
-        sub_ans = self.getPermutationRecursively(l, k)
+        ans = self.getPermutationRecursively(n, l, k, 1)
 
-        return ''.join([str(i) for i in sub_ans[k-1]])
+        return ans
--- 60.permutation-sequence/test_solution.py
+++ 60.permutation-sequence/test_solution.py
@@ -5,3 +5,15 @@ def test_1():
     s = Solution()
     ans = s.getPermutation(3, 3)
     assert ans == '213'
+
+
+def test_2():
+    s = Solution()
+    ans = s.getPermutation(2, 1)
+    assert ans == '12'
+
+
+def test_3():
+    s = Solution()
+    ans = s.getPermutation(3, 1)
+    assert ans == '123'
