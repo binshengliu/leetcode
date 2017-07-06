2161b30eded4acd721604efd924626e503fc2ff1
Binsheng Liu <lbsx@users.noreply.github.com>
2017-07-06 09:16:09 +0800

    solve #96 using bottom-up dp

--- 96.unique-binary-search-trees/solution.py
+++ 96.unique-binary-search-trees/solution.py
@@ -1,24 +1,15 @@
 class Solution(object):
-    def __init__(self):
-        self.cache = {}
-
     def numTrees(self, n):
         """
         :type n: int
         :rtype: int
         """
-        if n == 0:
-            return 1
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
 
-        if n == 1:
-            return 1
+        for i in range(2, n+1):
+            for j in range(0, i):
+                dp[i] += dp[j] * dp[i - j - 1]
 
-        if n in self.cache:
-            return self.cache[n]
-
-        ans = 0
-        for i in range(1, n+1):
-            ans += self.numTrees(i - 1) * self.numTrees(n - i)
-
-        self.cache[n] = ans
-        return ans
+        return dp[n]
