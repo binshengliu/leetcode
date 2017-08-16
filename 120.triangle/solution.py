0ccbe59ae5ce9fc5ca3813eb39e73f6c16342470
Binsheng Liu <lbsx@users.noreply.github.com>
2017-08-16 14:03:39 +0800

    solve #120 using memoization

--- 120.triangle/solution.py
+++ 120.triangle/solution.py
@@ -1,11 +1,17 @@
 class Solution(object):
     def minimumTotalRecursive(self, triangle, row, col):
+        if (row, col) in self.hashmap:
+            return self.hashmap[(row, col)]
+
         if row == len(triangle) - 1:
             return triangle[row][col]
 
-        return triangle[row][col] + \
+        value = triangle[row][col] + \
             min(self.minimumTotalRecursive(triangle, row+1, col),
                 self.minimumTotalRecursive(triangle, row+1, col+1))
+
+        self.hashmap[(row, col)] = value
+        return value
 
     def minimumTotal(self, triangle):
         """
@@ -15,4 +21,5 @@ class Solution(object):
         if not triangle:
             return None
 
+        self.hashmap = {}
         return self.minimumTotalRecursive(triangle, 0, 0)
