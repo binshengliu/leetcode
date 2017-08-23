1a772299cf187a90bf3c802c05765756ee6a1f21
Binsheng Liu <lbsx@users.noreply.github.com>
2017-08-23 16:17:53 +0800

    solve #126 using a dict

--- 126.word-ladder-ii/solution.py
+++ 126.word-ladder-ii/solution.py
@@ -1,19 +1,9 @@
+import string
+from collections import defaultdict
+
+
 class Solution(object):
-    def isOneCharDiff(self, word1, word2):
-        oneCharDiff = False
-        for (a, b) in zip(word1, word2):
-            if a == b:
-                continue
-
-            # a != b
-            if oneCharDiff:
-                return False
-            else:
-                oneCharDiff = True
-
-        return oneCharDiff
-
-    def getAllPaths(self, graph, begin, end):
+    def get_all_paths(self, graph, begin, end):
         # print("Constructing {} -> {}".format(begin, end))
         if begin == end:
             return [[end]]
@@ -23,7 +13,7 @@ class Solution(object):
 
         ans = []
         for word in graph[begin]:
-            sub_ans = self.getAllPaths(graph, word, end)
+            sub_ans = self.get_all_paths(graph, word, end)
             if not sub_ans:
                 continue
 
@@ -33,6 +23,34 @@ class Solution(object):
 
         return ans
 
+    def create_next_level_graph(self, word, word_set, graph, next_level_set):
+        for i in range(len(word)):
+            for ch in string.ascii_lowercase:
+                if ch == word[i]:
+                    continue
+                new_word = word[:i] + ch + word[i+1:]
+                if new_word in word_set:
+                    graph[word].add(new_word)
+                    next_level_set.add(new_word)
+
+    def create_graph(self, beginWord, endWord, word_set):
+        graph = defaultdict(set)
+        current_level_set = set([beginWord])
+        while current_level_set:
+            next_level_set = set()
+            word_set.difference_update(current_level_set)
+            for current in current_level_set:
+                # print("Queue: {}".format(current_level_set))
+
+                self.create_next_level_graph(current, word_set, graph, next_level_set)
+
+            if endWord in next_level_set:
+                break
+            current_level_set = next_level_set
+            # print("")
+
+        return graph
+
     def findLadders(self, beginWord, endWord, wordList):
         """
         :type beginWord: str
@@ -40,31 +58,14 @@ class Solution(object):
         :type wordList: List[str]
         :rtype: List[List[str]]
         """
-        wordSet = set(wordList)
-        if endWord not in wordSet:
+        word_set = set(wordList)
+        if endWord not in word_set:
             return []
 
-        graph = {}
-        bfsSet = set([beginWord])
-        while bfsSet:
-            nextLevelQueue = set()
-            wordSet.difference_update(bfsSet)
-            for current in bfsSet:
-                # print("Queue: {}".format(bfsSet))
-                if current == endWord:
-                    break
-
-                for word in wordSet:
-                    if self.isOneCharDiff(current, word):
-                        # print("key: {}, value: {}".format(current, word))
-                        graph.setdefault(current, []).append(word)
-                        nextLevelQueue.add(word)
-
-            bfsSet = nextLevelQueue
-            # print("")
+        graph = self.create_graph(beginWord, endWord, word_set)
 
         # print("Graph: {}".format(graph))
         ans = []
         if graph:
-            ans = self.getAllPaths(graph, beginWord, endWord)
+            ans = self.get_all_paths(graph, beginWord, endWord)
         return ans
